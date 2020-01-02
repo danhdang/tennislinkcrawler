07c8dd88d020d5c5d38f932f8b933dfc89a7a874
Danh Dang <ddang@tanchau.com>
2020-01-02 08:37:37 -0800

    add date parsing.

--- src/main/java/tennislink/crawler/parsers/TournamentDatesParser.java
+++ src/main/java/tennislink/crawler/parsers/TournamentDatesParser.java
@@ -2,13 +2,18 @@ package tennislink.crawler.parsers;
 
 import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tennislink.crawler.models.ParsedResult;
-
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Pattern;
 
-public class DatesParser {
+public class TournamentDatesParser {
+
+    private static final Logger log = LoggerFactory.getLogger(TournamentDatesParser.class);
 
     public void parseDateFields(ParsedResult parsedResult) {
         parsedResult.getParsedTournamentList().forEach(parsedTournament -> {
@@ -17,8 +22,8 @@ public class DatesParser {
             if(StringUtils.isNotEmpty(parsedTournament.getTournamentStartDate())) {
                 // looks like "01/04/2020"
                 DateTimeFormatter f = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-                LocalDate startDate = LocalDate.from(f.parse(parsedTournament.getTournamentStartDate()));
-                parsedTournament.setTournamentStartDate(startDate.toString());
+                LocalDate date = LocalDate.from(f.parse(parsedTournament.getTournamentStartDate()));
+                parsedTournament.setTournamentStartDate(date.atStartOfDay().toInstant(ZoneOffset.UTC).toString());
             }
 
             // Entries closed
@@ -28,17 +33,22 @@ public class DatesParser {
                 Pattern dateWithoutTIme = Pattern.compile("\\w+,\\s*\\w+\\s+\\d+,\\s*\\d{4}\\s*$");
                 if(dateWithTime.matcher(parsedTournament.getEntriesClosed()).find()) {
                     //looks like Tuesday, January 07, 2020 4:00 PM (Pacific Time)
-                    DateTimeFormatter f = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy  hh:mm (zzz)");
-                    LocalDate startDate = LocalDate.from(f.parse(parsedTournament.getEntriesClosed()));
-                    parsedTournament.setTournamentStartDate(startDate.toString());
+                    DateTimeFormatter f = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy  h:mm a z");
+                    String fixedTimeZone = fixTimeZone(parsedTournament.getEntriesClosed());
+                    LocalDateTime date = LocalDateTime.from(f.parse(fixedTimeZone));
+                    parsedTournament.setTournamentStartDate(date.toInstant(ZoneOffset.UTC).toString());
 
                 } else if (dateWithoutTIme.matcher(parsedTournament.getEntriesClosed()).find()) {
                     //looks like "Tuesday, December 31, 2019"
                     DateTimeFormatter f = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy");
-                    LocalDate startDate = LocalDate.from(f.parse(parsedTournament.getEntriesClosed()));
-                    parsedTournament.setTournamentStartDate(startDate.toString());
+                    LocalDate date = LocalDate.from(f.parse(parsedTournament.getEntriesClosed()));
+                    parsedTournament.setEntriesClosed(date.atStartOfDay().toInstant(ZoneOffset.UTC).toString());
                 }
             }
         });
+    }
+
+    private String fixTimeZone(String dateString) {
+        return dateString.replace("(Pacific Time)", "PST");
     }
 }
--- src/test/java/DateParserTest.java
+++ src/test/java/DateParserTest.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
+import tennislink.crawler.models.ParsedResult;
+import tennislink.crawler.models.ParsedTournament;
+import tennislink.crawler.parsers.TournamentDatesParser;
 
 public class DateParserTest {
+
+    @Test
+    public void testSimpleDate() {
+        ParsedResult result = new ParsedResult();
+        ParsedTournament tournament1 = new ParsedTournament();
+        tournament1.setTournamentId("123123123123");
+        tournament1.setTournamentName("This is my tournament");
+        tournament1.setChecksPayableTo("Payable to me");
+        tournament1.setCity("San Diego, CA");
+        tournament1.setTournamentId("123123123123");
+        tournament1.setDirectorCell("671-123-1231");
+        tournament1.setDirectorName("Mr. Director");
+        tournament1.setDivisions(new String[] { "division 1", "division 2"});
+        tournament1.setTournamentStartDate("01/04/2020");
+        tournament1.setEntriesClosed("Tuesday, December 31, 2019");
+        result.add(tournament1);
+
+        TournamentDatesParser parser = new TournamentDatesParser();
+        parser.parseDateFields(result);
+
+        Assert.assertEquals("2020-01-04T00:00:00Z", tournament1.getTournamentStartDate());
+        Assert.assertEquals("2019-12-31T00:00:00Z", tournament1.getEntriesClosed());
+
+        System.out.println(tournament1.getTournamentStartDate());
+        System.out.println(tournament1.getEntriesClosed());
+
+    }
+
+    @Test
+    public void testComplexDate() {
+        ParsedResult result = new ParsedResult();
+        ParsedTournament tournament1 = new ParsedTournament();
+        tournament1.setTournamentId("123123123123");
+        tournament1.setTournamentName("This is my tournament");
+        tournament1.setChecksPayableTo("Payable to me");
+        tournament1.setCity("San Diego, CA");
+        tournament1.setTournamentId("123123123123");
+        tournament1.setDirectorCell("671-123-1231");
+        tournament1.setDirectorName("Mr. Director");
+        tournament1.setDivisions(new String[] { "division 1", "division 2"});
+        tournament1.setTournamentStartDate("01/04/2020");
+        tournament1.setEntriesClosed("Tuesday, January 07, 2020 11:59 PM (Pacific Time)");
+        result.add(tournament1);
+
+        TournamentDatesParser parser = new TournamentDatesParser();
+        parser.parseDateFields(result);
+
+        Assert.assertEquals("2020-01-04T00:00:00Z", tournament1.getTournamentStartDate());
+
+        System.out.println(tournament1.getTournamentStartDate());
+        System.out.println(tournament1.getEntriesClosed());
+
+    }
 }
